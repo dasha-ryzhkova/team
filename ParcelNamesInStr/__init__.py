a3912dfe24866e8ddd7b83132137a76557fb2759
asya <qwerty.com>
2019-11-01 11:48:51 +0200

    ParcelNamesInStr

---
+++ ParcelNamesInStr/__init__.py
@@ -0,0 +1,9 @@
+def ParcelNamesInStr(your_names):
+    '''
+    Функція перетворює рядок з ПІБ у 3, де прізвище, ім'я, побатькові виводиться у кожному окремо.
+    :param your_names: type(str), format(ПІБ), in(input), example(Рижкова Дар'я Олександрівна)
+    :return: type(str),format(Прізвище        example(Рижкова
+                              Ім'я                    Дар'я
+                              Побатькові)             Олександрівна)
+    '''
+
